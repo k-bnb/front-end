c0d10afbf737a2e26f68648181fa978ce7b5e3b5
yongmin kim <kym930722@gmail.com>
2021-02-05 20:56:03 +0900

    feat: search module state 변경
    search module의 state의 구조 변경에 따른 reducer 함수 로직 변경.

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,7 +1,8 @@
-import { createAction, handleActions } from "redux-actions";
-import createRequestSaga from "../lib/createRequestSaga";
-import * as API from "../lib/api/search";
-import { takeLatest } from "redux-saga/effects";
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga from '../lib/createRequestSaga';
+import * as API from '../lib/api/search';
+import { takeLatest } from 'redux-saga/effects';
+import produce from 'immer';
 
 //action type
 //비동기x
@@ -17,79 +18,106 @@ const SEARCHING_SUCCESS = 'search/SEARCHING_SUCCESS';
 const SEARCHING_FAILURE = 'search/SEARCHINGSEARCHING_FAILURE';
 
 //action create
-export const destinationInput = createAction(DESTINATION_INPUT, destinationName => destinationName) //destinationName 문자열.
-export const locationInput = createAction(LOCATION_INPUT, locationSearch=>locationSearch) //locationSearch 객체.
-export const dateInput = createAction(DATE_INPUT,checkDateSearch => checkDateSearch) //checkDateSearch 객체.
-export const guestInput = createAction(GUEST_INPUT, guestSearch => guestSearch) //guestSearch 객체.
-export const searching = createAction(SEARCHING, ({locationSearch,checkDateSearch,guestSearch}) => ({
-  locationSearch,
-  checkDateSearch,
-  guestSearch
-}));
-
+export const destinationInput = createAction(
+  DESTINATION_INPUT,
+  (destinationName) => destinationName,
+); //destinationName 문자열.
+export const locationInput = createAction(
+  LOCATION_INPUT,
+  (locationSearch) => locationSearch,
+); //locationSearch 객체.
+export const dateInput = createAction(
+  DATE_INPUT,
+  (checkDateSearch) => checkDateSearch,
+); //checkDateSearch 객체.
+export const guestInput = createAction(
+  GUEST_INPUT,
+  (guestSearch) => guestSearch,
+); //guestSearch 객체.
+export const searching = createAction(
+  SEARCHING,
+  ({ locationSearch, checkDateSearch, guestSearch }) => ({
+    locationSearch,
+    checkDateSearch,
+    guestSearch,
+  }),
+);
 
 //initial State
 const initialState = {
-  destinationName:'',
-  locationSearch:{
-    latitude: null,
-    longitude: null,
-    latitudeMax:null,
-    latitudeMin:null,
-    longitudeMax:null,
-    longitudeMin: null,
+  destinationName: '',
+  searchReq: {
+    locationSearch: {
+      latitude: null,
+      longitude: null,
+      latitudeMax: null,
+      latitudeMin: null,
+      longitudeMax: null,
+      longitudeMin: null,
+    },
+    checkDateSearch: {
+      startDate: '',
+      endDate: '',
+    },
+    guestSearch: {
+      numOfAdult: 0,
+      numOfKid: 0,
+      numOfInfant: 0,
+    },
   },
-  checkDateSearch:{
-    startDate:'',
-    endDate:'',
-  },
-  guestSearch:{
-    numOfAdult:0,
-    numOfKid:0,
-    numOfInfant:0,
-  },
-  searchError:null,
-}
+  searchRes: [],
+  searchError: null,
+};
+
 //saga
 const searchingSaga = createRequestSaga(SEARCHING, API.search);
 
 //seachSaga
-export function* searchSaga(){
+export function* searchSaga() {
   yield takeLatest(SEARCHING, searchingSaga);
 }
 
 //Reducer
 const search = handleActions(
   {
-    [DESTINATION_INPUT]: (state, {payload:destinationName})=>({
+    [DESTINATION_INPUT]: (state, { payload: destinationName }) => ({
       ...state,
       destinationName,
     }),
 
-    [LOCATION_INPUT] : (state, {payload:{ locationSearch }}) => ({
-      ...state,
-      locationSearch,
-    }),
+    [LOCATION_INPUT]: (state, { payload: { locationSearch } }) =>
+      produce(state, (draft) => {
+        draft.searchReq.locationSearch = locationSearch;
+      }),
 
-    [DATE_INPUT] : (state, {payload:{checkDateSearch}})=>({
-      ...state,
-      checkDateSearch,
-      // checkDateSearch: action.payload.checkDateSearch
-    }),
-    [GUEST_INPUT] : (state, {payload:{guestSearch}}) => ({
-      ...state,
-      guestSearch,
-    }),
-    
-    [SEARCHING_SUCCESS] : (state, {payload:{_embedded:{roomDtoList}}})=>({
-      ...state,
-      room:{roomDtoList}
-    }), //_embedded:{roomDtoList} 는 방 이름.
-    [SEARCHING_FAILURE] : (_, {payload:{error}})=>({
+    [DATE_INPUT]: (state, { payload: { checkDateSearch } }) =>
+      produce(state, (draft) => {
+        draft.searchReq.checkDateSearch = checkDateSearch;
+      }),
+
+    [GUEST_INPUT]: (state, { payload: { guestSearch } }) =>
+      produce(state, (draft) => {
+        draft.searchReq.guestSearch = guestSearch;
+      }),
+
+    [SEARCHING_SUCCESS]: (
+      state,
+      {
+        payload: {
+          _embedded: { roomDtoList },
+        },
+      },
+    ) =>
+      produce(state, (draft) => {
+        draft.searchRes = roomDtoList;
+      }), //_embedded:{roomDtoList} 는 방 정보를 가진 배열.
+
+    [SEARCHING_FAILURE]: (_, { payload: { error } }) => ({
       ...initialState,
-      searchError:error,
-    })
-  }, initialState
-)
+      searchError: error,
+    }),
+  },
+  initialState,
+);
 
 export default search;
