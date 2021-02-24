72dedb4412ca96c299ed0060a44fc25142fc4148
yongmin kim <kym930722@gmail.com>
2021-02-24 21:25:53 +0900

    fix: backend api 주소 변경

--- src/lib/api/detail.js
+++ src/lib/api/detail.js
@@ -1,17 +1,14 @@
 import client from './client';
 
 export const detailInformation = (roomId) =>
-  client.get(
-    `https://kbnb-backend.herokuapp.com/room/detail?roomId=${roomId}`,
-    {
-      headers: {
-        Authorization: `Bearer`,
-        'Content-Type': 'application/json',
-      },
+  client.get(`https://backend.kbnb.tk/room/detail?roomId=${roomId}`, {
+    headers: {
+      Authorization: `Bearer`,
+      'Content-Type': 'application/json',
     },
-  );
+  });
 
 export const requestComments = (roomId, pageNum) =>
   client.get(
-    `https://kbnb-backend.herokuapp.com/comment?roomId=${roomId}&page=${pageNum}&size=6`,
+    `https://backend.kbnb.tk/comment?roomId=${roomId}&page=${pageNum}&size=6`,
   );
