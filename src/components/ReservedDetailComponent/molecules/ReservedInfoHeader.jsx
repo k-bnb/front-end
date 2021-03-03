36f340cbbcf92396d0f797a4c76021f4d130b2d4
park-moen <pmj6516@naver.com>
2021-03-03 17:36:48 +0900

    feat: 예약한 방 정보 상세 보기 UI 일부 구현

---
+++ src/components/ReservedDetailComponent/molecules/ReservedInfoHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.header``;
+
+const ReservedInfoHeader = () => {
+  return (
+    <Title>
+      <h1>숙소 예약</h1>
+    </Title>
+  );
+};
+
+export default ReservedInfoHeader;
